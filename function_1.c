3f336fc5c220a313c1beff88a01fb0ceec3e96cf
Ratan-gupta07 <ratanbxr2000@gmail.com>
2025-12-24 23:53:35 +0530

    Function Program 1

---
+++ function_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void printhello();  //Function
+void printgoodbye();
+
+int main(){
+    printhello();  //function call
+    printgoodbye();
+}
+
+   //function defination
+void printhello(){
+    printf("Hello Babu\n");
+
+}
+void printgoodbye(){
+    printf("Hello Babu\n");
+    
+}
